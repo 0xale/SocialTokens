dd482cb83a48e457f1b386c1029195f2e9577d44
0xale <0xalex.eth@gmail.com>
2024-08-11 21:07:32 +0530

    update description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import homeStyle from "./Styles/HomePage.module.css";
 
-
 export default function Home() {
   return (
     <div className={homeStyle.container}>
       <h1 className={homeStyle.header}>Social Tokens</h1>
       <div className={homeStyle.image}>
-      Our platform enables users to create social tokens, leveraging a bonding curve mechanism to dynamically set their value. As more tokens are issued and Farcaster follower counts increase, the value of each token rises . additonally bonus tokens are minted to users using Pyth Oracal by genrating random percentage from ETHUSDPriceFeed This system facilitates user engagement and enhances token value through its growth-based pricing model.
+        <p>
+          Our platform empowers you to <strong>create social tokens</strong> with ease, using a <strong>bonding curve mechanism</strong> that dynamically adjusts their value. As more tokens are issued and your <strong>Farcaster follower count</strong> grows, the value of each token increases. Plus, we add an exciting twist: <strong>bonus tokens</strong> are minted through the <strong>Pyth Oracal</strong>, generating random percentages from the <strong>ETH/USD price feed</strong>. This innovative system not only <strong>boosts user engagement</strong> but also <strong>enhances token value</strong> through its growth-based pricing model.
+        </p>
       </div>
     </div>
   );
